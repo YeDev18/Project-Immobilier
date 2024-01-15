d0f27551688b1130c55c25f523cef8fd8293846d
Emmanuel252 <yedev@YeDev>
2024-01-15 21:10:09 +0000

    Edit All 1

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <section className="footer">
+      <div className="footer_div">
+        {' '}
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book.
+        </p>
+        <p>© 2024 Homelum. Designed by YeDev</p>
+      </div>
+    </section>
+  );
+};
+
+export default Footer;
